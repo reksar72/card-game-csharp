478094214d521485dfda53b8d85bc6461fbf0929
reksar72 <reksar72@icloud.com>
2025-10-06 16:31:05 +0300

    Create Game.cs

---
+++ Game.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Game
+{
+    public Player Player1;
+    public Player Player2;
+    public Player CurrentPlayer;
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
